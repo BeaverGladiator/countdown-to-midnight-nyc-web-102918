99eef618c903afa0f8ddc675be6b2328f9411e03
Matthew Migliore <mmigliore311@gmail.com>
2018-10-10 14:20:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
 #write your code here
 
-def countdown(start)
-  x = 1
-  while start > 0
-    puts "#{start} is less than 10"
-    start -= 1
+def countdown(number)
+  while number > 0
+    puts "#{number} is less than 10"
+    number -= 1
   end
 end
